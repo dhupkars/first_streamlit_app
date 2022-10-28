9270907dddcefda69e8fde1b205b3beca479986c
dhupkars <dhupkarsanjeev@hotmail.com>
2022-10-28 11:17:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents new Healthy Dinner")
+streamlit.header("Breakfast Menu!")
+streamlit.text("Chocolate Chip Oatmeal!")
+streamlit.text("Kande Batate Pohe")
+streamlit.text("Omelette Bread")
