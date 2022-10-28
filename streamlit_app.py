94bea5504b551de26d5cff7e729efd148b35d178
dhupkars <dhupkarsanjeev@hotmail.com>
2022-10-28 11:20:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
+import pandas
+
 streamlit.title("My Parents new Healthy Dinner")
 streamlit.header("Breakfast Menu!")
 streamlit.text("🥣 Chocolate Chip Oatmeal!")
 streamlit.text("Kande Batate Pohe")
 streamlit.text("Omelette Bread")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
