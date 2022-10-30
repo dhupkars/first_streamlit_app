f7a9b0ea0bd433b0164d4b6b0b35613985015bb5
dhupkars <dhupkarsanjeev@hotmail.com>
2022-10-30 17:30:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.text("Fruit Load list:")
 streamlit.dataframe(my_data_row)
